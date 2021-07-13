220ce2aeac571f11f54534da433d358203bbd328
NKaty <nd.katy@gmail.com>
2021-07-13 22:28:31 +0300

    Fix header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,53 @@
 import { Link, NavLink } from 'react-router-dom';
+import { Component } from 'react';
 
-const Header = () => {
-  return (
-    <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <Link to="/customers" className="navbar-brand">
-          Customer Library
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <div className="navbar-nav">
-            <NavLink
-              exact
-              to="/customers"
-              className="nav-link"
-              activeClassName="active"
-            >
-              Customers
-            </NavLink>
+class Header extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  onToggleMenu = event => {
+    event.preventDefault();
+    this.setState(prevSate => ({ isOpen: !prevSate.isOpen }));
+  };
+
+  render() {
+    return (
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <Link to="/customers" className="navbar-brand">
+            Customer Library
+          </Link>
+          <button
+            className="navbar-toggler"
+            onClick={this.onToggleMenu}
+            type="button"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div
+            className={
+              'navbar-collapse' + (!this.state.isOpen ? ' collapse' : '')
+            }
+            id="navbarNav"
+          >
+            <div className="navbar-nav">
+              <NavLink
+                exact
+                to="/customers"
+                className="nav-link"
+                activeClassName="active"
+              >
+                Customers
+              </NavLink>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
-  );
-};
+      </nav>
+    );
+  }
+}
 
 export default Header;
